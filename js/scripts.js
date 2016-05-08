5c389715713a8027b54cd442599b175ac149f079
jeremyukim <jeremyukim@gmail.com>
2016-05-08 18:07:40 -0400

    Added animations and improved background movement

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var movementStrength = 20;
+  var movementStrength = 30;
   var height = movementStrength / $(window).height();
   var width = movementStrength / $(window).width();
   $("#landingPageBackground").mousemove(function(e) {
@@ -11,16 +11,7 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
-  var movementStrength = 30;
-  var height = movementStrength / $(window).height();
-  var width = movementStrength / $(window).width();
-  $("#landingPageText").mousemove(function(e) {
-    var pageX = e.pageX - ($(window).width() / 2);
-    var pageY = e.pageY - ($(window).height() / 2);
-    var newvalueX = width * pageX * -1;
-    var newvalueY = height * pageY * -1;
-    $('#landingPageText').css("padding-left", newvalueX + "px     ");
-    $('#landingPageText').css("padding-top", newvalueY + "px     ");
-  });
+$(window).load(function() {
+  $("#name1").css("display", "inline-block").addClass("animated pulse");
+  $("#name2").css("display", "inline-block").addClass("animated fadeIn");
 });
