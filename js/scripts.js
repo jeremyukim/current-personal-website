7e3e6088e64b8c0a5acbff968d8bc919729e571d
jeremyukim <jeremyukim@gmail.com>
2016-05-08 16:50:08 -0400

    Fixed repeating background issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var movementStrength = 25;
+  var movementStrength = 20;
   var height = movementStrength / $(window).height();
   var width = movementStrength / $(window).width();
   $("#landingPageBackground").mousemove(function(e) {
@@ -8,5 +8,19 @@ $(document).ready(function() {
     var newvalueX = width * pageX * -1 - 25;
     var newvalueY = height * pageY * -1 - 50;
     $('#landingPageBackground').css("background-position", newvalueX + "px     " + newvalueY + "px");
+  });
+});
+
+$(document).ready(function() {
+  var movementStrength = 30;
+  var height = movementStrength / $(window).height();
+  var width = movementStrength / $(window).width();
+  $("#landingPageText").mousemove(function(e) {
+    var pageX = e.pageX - ($(window).width() / 2);
+    var pageY = e.pageY - ($(window).height() / 2);
+    var newvalueX = width * pageX * -1;
+    var newvalueY = height * pageY * -1;
+    $('#landingPageText').css("padding-left", newvalueX + "px     ");
+    $('#landingPageText').css("padding-top", newvalueY + "px     ");
   });
 });
